64e4bda81360e6e56735df0f990c039d242811ba
aish8la <64914670+aish8la@users.noreply.github.com.>
2025-03-21 22:21:26 +0500

    Add method to calculate possible moves

--- src/knights-travails.js
+++ src/knights-travails.js
@@ -9,12 +9,27 @@ class KnightsTravails {
         return Array.from({length: size}, () => null);
     }
 
+    possibleVertices(current) {
+        const [x, y] = current;
+        const vertices = [];
+        this.movements.forEach(e => {
+            const newVertex = [x + e[0], y + e[1]];
+            
+            if(newVertex[0] > this.maxBound[0] || newVertex[1] > this.maxBound[1]) return;
+            if(newVertex[0] < this.minBound[0] || newVertex[1] < this.minBound[1]) return;
+            vertices.push(newVertex);
+        });
+        return vertices;
+    }
+
     knightMoves(source, target) {
         const q = [source];
         const visited = Array.from({length: this.maxBound[1]}, () => this.createNullArray(this.maxBound[0]));
-        console.log(visited);
+        while(q.length > 0) {
+
+        }
     }
 }
 
 const knights = new KnightsTravails();
-console.log(knights.knightMoves(7));
+console.log(knights.possibleVertices([0,0]));
